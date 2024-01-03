ee1254ac77aa05534a87279abe78dc0c6c17ac8a
Caleb Heuer <ch@raiguard.me>
2024-01-03 16:47:05 -0700

    Centered red rectangle

--- src/window.cpp
+++ src/window.cpp
@@ -6,13 +6,11 @@
 Window::Window(const char* title)
 {
   SDL_SetHint(SDL_HINT_EVENT_LOGGING, "1");
-  SDL_SetHint(SDL_HINT_JOYSTICK_ALLOW_BACKGROUND_EVENTS, "1");
-  SDL_SetHint(SDL_HINT_GAMECONTROLLER_USE_BUTTON_LABELS, "1");
   SDL_SetHint(SDL_HINT_VIDEODRIVER, "wayland,x11");
   if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
     throw std::runtime_error("Failed to initialize SDL");
 
-  this->window = SDL_CreateWindow(title, SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 0, 0, SDL_WINDOW_SHOWN);
+  this->window = SDL_CreateWindow(title, 0, 0, 1920, 1080, SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
   this->refreshSurface();
   this->logEnv();
   this->logDisplayInfo();
@@ -27,7 +25,11 @@ Window::~Window()
 void Window::refreshSurface()
 {
   this->surface = SDL_GetWindowSurface(this->window);
-  SDL_FillRect(this->surface, NULL, SDL_MapRGB(this->surface->format, 0, 0, 0));
+  SDL_FillRect(this->surface, nullptr, SDL_MapRGB(this->surface->format, 0, 0, 0));
+  int width = this->surface->w / 3;
+  int height = this->surface->h / 3;
+  SDL_Rect fillRect = {this->surface->w / 2 - width / 2, this->surface->h / 2 - height / 2, width, height};
+  SDL_FillRect(this->surface, &fillRect, SDL_MapRGB(this->surface->format, 255, 0, 0));
   SDL_UpdateWindowSurface(this->window);
 }
 
